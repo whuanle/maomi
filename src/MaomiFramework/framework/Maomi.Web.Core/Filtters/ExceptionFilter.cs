1166a6281e9989cdb76f9e7f1d9ab8ab7119f58d
痴者工良 <1586052146@qq.com>
2023-07-05 06:50:47 +0800

    20230705

---
+++ src/MaomiFramework/framework/Maomi.Web.Core/Filtters/ExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Maomi.Web.Core.Filtters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<ExceptionFilter> _logger;
+
+        public ExceptionFilter(ILogger<ExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception,
+                """
+                Request URL {1}
+                """
+                , context.HttpContext.Request.GetDisplayUrl());
+        }
+    }
+
+}
