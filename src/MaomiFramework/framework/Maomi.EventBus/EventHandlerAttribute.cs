3418f3e15d7793091416e57c2aa4866fa188c9c8
whuanle <1586052146@qq.com>
2023-08-16 20:07:08 +0800

    1

--- src/MaomiFramework/framework/Maomi.EventBus/EventHandlerAttribute.cs
+++ src/MaomiFramework/framework/Maomi.EventBus/EventHandlerAttribute.cs
@@ -1,17 +1,25 @@
 ﻿namespace Maomi.EventBus
 {
-    // 标识类中有事件执行器
+    /// <summary>
+    /// 标识类中有事件执行器
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class EventAttribute : Attribute { }
 
-    // 标识方法是一个事件执行器
+    /// <summary>
+    /// 标识方法是一个事件执行器
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public class EventHandlerAttribute : Attribute
     {
-        // 事件排序
+        /// <summary>
+        /// 事件排序
+        /// </summary>
         public int Order { get; set; } = 0;
 
-        // 是否为撤销事件
+        /// <summary>
+        /// 是否为补偿事件
+        /// </summary>
         public bool IsCancel { get; set; } = false;
     }
 }
