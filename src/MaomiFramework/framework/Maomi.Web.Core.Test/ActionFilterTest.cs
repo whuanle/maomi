04072f1a1770bd5f51372c0c6065d2d507a9d64e
whuanle <1586052146@qq.com>
2023-09-10 20:00:07 +0800

    1

--- src/MaomiFramework/framework/Maomi.Web.Core.Test/ActionFilterTest.cs
+++ src/MaomiFramework/framework/Maomi.Web.Core.Test/ActionFilterTest.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Maomi.I18n;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 
 namespace Maomi.Web.Core.Test
 {
-    internal class ActionFilterTest
+    public class ActionFilterTest
     {
+        // 测试在中英文情况下，模型验证失败时返回统一格式
+        [Fact]
+        public async Task Request_I18n()
+        {
+            using var host = await new HostBuilder()
+                .ConfigureWebHost(webBuilder =>
+                {
+                    webBuilder
+                        .UseTestServer()
+                        .ConfigureServices(services =>
+                        {
+                            services.AddControllers();
+                            services.AddI18n(defaultLanguage: "zh-CN");
+                            services.AddI18nResource(option =>
+                            {
+                                var basePath = "i18n";
+                                option.AddJson<I18nTest>(basePath);
+                            });
+                        })
+                        .Configure(app =>
+                        {
+                            app.UseI18n();
+                            app.UseRouting();
+                            app.MapPost("/test", ([FromBody] DemoModel model) =>
+                            {
+                                return "test";
+                            });
+                        });
+                })
+                .StartAsync();
+
+
+
+            var httpClient = host.GetTestClient();
+            httpClient.DefaultRequestHeaders.AcceptLanguage.Clear();
+            var response = await httpClient.PostAsync("/test");
+            Assert.Equal("Product name", response);
+
+            httpClient.DefaultRequestHeaders.AcceptLanguage.Clear();
+            response = await httpClient.GetStringAsync("/test?culture=en-US&ui-culture=en-US");
+            Assert.Equal("Product name", response);
+
+            response = await httpClient.GetStringAsync("/test?culture=zh-CN&ui-culture=zh-CN");
+            Assert.Equal("商品名称", response);
+        }
+    }
+    public class DemoModel
+    {
+
     }
 }
