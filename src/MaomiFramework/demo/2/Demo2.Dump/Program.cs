8ec3f8d409b5e7c8af8ca1d3ae1d08d0d7646a26
痴者工良 <1586052146@qq.com>
2023-09-15 11:19:43 +0000

    1

---
+++ src/MaomiFramework/demo/2/Demo2.Dump/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Diagnostics.NETCore.Client;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+
+public class Program
+{
+    static async Task Main()
+    {
+        var processes = DiagnosticsClient.GetPublishedProcesses()
+        .Select(Process.GetProcessById)
+        .Where(process => process != null);
+        Console.WriteLine("请输入进程 id");
+        foreach (var item in processes)
+        {
+            Console.WriteLine($"{item.Id} ------ {item.ProcessName}");
+        }
+
+        var read = Console.ReadLine();
+        ArgumentNullException.ThrowIfNullOrEmpty(read);
+        var pid = int.Parse(read);
+
+        var client = new DiagnosticsClient(pid);
+        await client.WriteDumpAsync(
+            dumpType: DumpType.Full,
+            dumpPath: $"D:/{pid}_{DateTime.Now.Ticks}.dmp",
+            logDumpGeneration: true,
+            token: CancellationToken.None
+        );
+    }
+}
