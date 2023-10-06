233fbfc36910fcb41f0f154e2e77fdd04e29054c
whuanle <1586052146@qq.com>
2023-10-06 10:34:14 +0800

    1

--- src/MaomiFramework/framework/Maomi.Web.Core/Swagger/MaomiSwaggerSchemaFilter.cs
+++ src/MaomiFramework/framework/Maomi.Web.Core/Swagger/MaomiSwaggerSchemaFilter.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.OpenApi.Extensions;
-using Microsoft.OpenApi.Models;
+﻿using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Reflection;
 using System.Text.Json.Serialization;
