6522d16d03ae78facabb2e91edc85f8cb35c7302
whuanle <1586052146@qq.com>
2023-11-06 19:38:34 +0800

    111

--- src/MaomiFramework/demo/6/Demo6.FullApi/Controllers/DefaultController.cs
+++ src/MaomiFramework/demo/6/Demo6.FullApi/Controllers/DefaultController.cs
@@ -12,112 +12,33 @@ namespace WebApplication2.Controllers
 	[ApiController]
 	public class DefaultController : ControllerBase
 	{
-		private readonly ITest ggg;
-		public DefaultController(ITest ttt)
+		//private readonly ITest ggg;
+		//public DefaultController(ITest ttt)
+		//{
+		//	ggg = ttt;
+		//}
+
+		public class AObj
 		{
-			ggg = ttt;
-		}
-		[HttpGet("aaa")]
-		public async Task<JsonResult> AAA(int? a, int? b)
-		{
-			if (a == null | b == null)
-				return new JsonResult(new { code = 0, result = "aaaaaaaa" });
-			return new JsonResult(new { code = 200, result = a + "|" + b });
-		}
-		public class AppJson { public int? a { get; set; } public int? b { get; set; } }
-		[HttpPost("bbb")]
-		public async Task<JsonResult> BBB([FromBody] AppJson ss)
-		{
-			if (ss.a == null || ss.b == null) return new JsonResult(new
-			{
-				code = 0,
-				result = "aaaaaaaa"
-			});
-			return new JsonResult(new { code = 2000, result = ss.a + "|" + ss.b });
+			public int B { get; set; }
 		}
 
-		public class AppJson
+		[HttpGet("ao")]
+		public async Task<int> AO(int? a, AObj obj)
 		{
-			public int? a { get; set; }
-			public int? b { get; set; }
-		}
-		[HttpPost("bbb")]
-		public async Task<JsonResult> BBB([FromBody] AppJson ss)
-		{
-			if (ss.a == null || ss.b == null) return new JsonResult(new { code = 0, result = "aaaaaaaa" });
-			return new JsonResult(new { code = 200, result = ss.a + "|" + ss.b });
+			return 0;
 		}
 
-		[HttpPost("ccc")]
-		public async Task<JsonResult> CCC([FromForm] AppJson ss)
+		[HttpGet("a")]
+		public async Task<int> A([FromQuery] int? a, [FromBody] AObj obj)
 		{
-			if (ss.a == null || ss.b == null)
-				return new JsonResult(new { code = 0, result = "aaaaaaaa" });
-			return new JsonResult(new { code = 200, result = ss.a + "|" + ss.b });
+			return 0;
 		}
-		[HttpPost("ddd")]
-		public async Task<JsonResult> DDD([FromHeader] int? a, [FromHeader] int? b)
+
+		[HttpGet("ac")]
+		public async Task<int> AC([FromQuery] int? a, [FromBody] AObj obj, CancellationToken cancellationToken)
 		{
-			if (a == null || b == null)
-				return new JsonResult(new { code = 0, result = "aaaaaaaa" });
-			return new JsonResult(new { code = 200, result = a + "|" + b });
-		}
-		[HttpPost("eee")]
-		public async Task<JsonResult> EEE([FromQuery] int? a, [FromQuery] int? b)
-		{
-			if (a == null || b == null) return new JsonResult(new { code = 0, result = "aaaaaaaa" });
-			return new JsonResult(new { code = 200, result = a + "|" + b });
-		}
-		[HttpPost("fff")]
-		public async Task<JsonResult> FFFxxx(int a, int b, [FromRoute] string controller, [FromRoute] string action)
-		{
-			// 这里就不处理 a和 b了
-			return new JsonResult(new { code = 200, result = controller + "|" + action });
-		}
-		[HttpPost("/ooo")]
-		public async Task<JsonResult> FFFooo(int a, int b, [FromRoute] string controller, [FromRoute] string action)
-		{
-			// 这里就不处理 a和 b了
-			return new JsonResult(new { code = 200, result = controller + "|" + action });
-		}
-		[HttpPost("ggg")]
-		public async Task<JsonResult> GGG([FromServices] ITest t)
-		{
-			return new JsonResult(new { code = 200, result = t.GGG });
-		}
-		public class TestBind
-		{
-			public string A { get; set; }
-			public string B { get; set; }
-			public string C { get; set; }
-			public string D { get; set; }
-			public string E { get; set; }
-			public string F { get; set; }
-			public string G { get; set; }
-		}
-		[HttpPost("hhh")]
-		public async Task<JsonResult> HHH(
-			string A, string B,
-			string E, string F, string G,
-			[Bind("A,B,C,D")] TestBind test,
-			 string C, string D,
-			 string J, string Q)
-		{
-			if (ModelState.IsValid == true)
-				return new JsonResult(new
-				{
-					data1 = test,
-					dataA = A,
-					dataB = B,
-					dataC = C,
-					dataD = D,
-					dataE = E,
-					dataF = F,
-					dataG = G,
-					dataJ = J,
-					dataQ = Q
-				});
-			return new JsonResult(new { Code = 0, Result = "验证不通过" });
+			return 0;
 		}
 	}
 }
